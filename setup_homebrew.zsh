569df17a884bc7bbefa93bd72fe2686bb090fbdd
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2023-11-04 17:54:07 +0100

    Bypass Gatekeeper for brewfile

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -3,4 +3,10 @@
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# TODO: Keep an eye out for a different `--no-quarantine` solution.
+# Currently, you can't do `brew bundle --no-quarantine` as an option.
+# It's currently exported in zshrc:
+# export HOMEBREW_CASK_OPTS="--no-quarantine"
+# https://github.com/homebrew/homebrew-bundle/issues/474
+
 brew bundle --verbose
