1c390532212641b8d582e315f3160a7124edccd0
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-03-31 21:53:17 +0200

    Use obsidian plugin for all markdown files not only obsidian vaults

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -9,6 +9,22 @@ return {
         name = "Paulaner",
         path = "~/Dev/Git/obsidian/Paulaner",
       },
+      { -- NOTE: Use obsidian bullet highlighting also for all MD files not only Obsidian workspaces
+        name = "no-vault",
+        path = function()
+          -- alternatively use the CWD:
+          -- return assert(vim.fn.getcwd())
+          return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+        end,
+        overrides = {
+          notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of nil
+          new_notes_location = "current_dir",
+          templates = {
+            subdir = vim.NILa,
+          },
+          disable_frontmatter = true,
+        },
+      },
     },
   },
 }
