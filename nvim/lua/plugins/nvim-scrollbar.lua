ce96ee0bcd9b0fd68bed10e21e3e2fe6472ab9f0
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-06-22 12:04:05 +0200

    Add nvim scrollbar functionality

---
+++ nvim/lua/plugins/nvim-scrollbar.lua
@@ -0,0 +1,16 @@
+return {
+  "petertriho/nvim-scrollbar",
+  opts = {
+    handle = {
+      color = "#6E738D",
+    },
+  },
+  -- INFO: Alternative way to configure the plugin
+  -- config = function()
+  --   require("scrollbar").setup({
+  --     handle = {
+  --       color = "#6E738D",
+  --     },
+  --   })
+  -- end,
+}
