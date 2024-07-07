8f076045845b79acc9dab675d1746bc6f3fe1ed7
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-07-07 12:13:25 +0200

    Add new bullets for markdown headlines

--- nvim/lua/plugins/headlines.lua
+++ nvim/lua/plugins/headlines.lua
@@ -2,7 +2,9 @@ return {
   "lukas-reineke/headlines.nvim",
   opts = {
     markdown = {
-      fat_headline_lower_string = "▔",
+      fat_headline_upper_string = "▄",
+      fat_headline_lower_string = "▀",
+      bullets = { "󰎤", "󰎧", "󰎪", "󰎭", "󰎱", "󰎳" },
     },
   },
 }
