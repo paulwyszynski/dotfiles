0054412e008384ebfc3ccd6cc43a993d0edd2885
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-07-07 10:45:05 +0200

    Add new descriptions for which-key

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -4,6 +4,8 @@ return {
     defaults = {
       [";"] = { desc = "Repeat f, t, F or T N times" },
       [","] = { desc = "Repeat f, t, F or T N times backwards" },
+      ["<C-t>"] = { desc = "Indent in insert mode" },
+      ["<C-d>"] = { desc = "Unindent in insert mode" },
     },
   },
 }
