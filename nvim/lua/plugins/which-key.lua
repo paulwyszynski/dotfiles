9c7f487e5b35e68c99f86d138e83fc0fff80d08b
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-07-21 20:15:12 +0200

    Disable custom key descriptions in which-key config

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,11 +1,15 @@
-return {
-  "folke/which-key.nvim",
-  opts = {
-    defaults = {
-      [";"] = { desc = "Repeat f, t, F or T N times" },
-      [","] = { desc = "Repeat f, t, F or T N times backwards" },
-      ["<C-t>"] = { desc = "Indent in insert mode" },
-      ["<C-d>"] = { desc = "Unindent in insert mode" },
-    },
-  },
-}
+-- New version of which-key has issues with this config. New defaults using instead.
+if true then
+  return {}
+end
+-- return {
+--   "folke/which-key.nvim",
+--   opts = {
+--     defaults = {
+--       { ",", desc = "Repeat f, t, F or T N times backwards" },
+--       { ";", desc = "Repeat f, t, F or T N times" },
+--       { "<C-d>", desc = "Unindent in insert mode" },
+--       { "<C-t>", desc = "Indent in insert mode" },
+--     },
+--   },
+-- }
