0f2fca51f48c8ed13bde342951d6c5864d556f5f
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-07-24 18:13:24 +0200

    Disabled markdown plugins because of new markdown.nvim

--- nvim/lua/plugins/headlines.lua
+++ nvim/lua/plugins/headlines.lua
@@ -1,10 +1,14 @@
-return {
-  "lukas-reineke/headlines.nvim",
-  opts = {
-    markdown = {
-      fat_headline_upper_string = "▄",
-      fat_headline_lower_string = "▀",
-      bullets = { "󰎤", "󰎧", "󰎪", "󰎭", "󰎱", "󰎳" },
-    },
-  },
-}
+-- Got obsolete since the new markdown.nvim plugin
+if true then
+  return {}
+end
+-- return {
+--   "lukas-reineke/headlines.nvim",
+--   opts = {
+--     markdown = {
+--       fat_headline_upper_string = "▄",
+--       fat_headline_lower_string = "▀",
+--       bullets = { "󰎤", "󰎧", "󰎪", "󰎭", "󰎱", "󰎳" },
+--     },
+--   },
+-- }
--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,30 +1,34 @@
-return {
-  "epwalsh/obsidian.nvim",
-  lazy = true,
-  ft = "markdown",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    workspaces = {
-      {
-        name = "Paulaner",
-        path = "~/Dev/Git/obsidian/Paulaner",
-      },
-      { -- NOTE: Use obsidian bullet highlighting also for all MD files not only Obsidian workspaces
-        name = "no-vault",
-        path = function()
-          -- alternatively use the CWD:
-          -- return assert(vim.fn.getcwd())
-          return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
-        end,
-        overrides = {
-          notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of nil
-          new_notes_location = "current_dir",
-          templates = {
-            subdir = vim.NILa,
-          },
-          disable_frontmatter = true,
-        },
-      },
-    },
-  },
-}
+-- Got obsolete since the new markdown.nvim plugin
+if true then
+  return {}
+end
+-- return {
+--   "epwalsh/obsidian.nvim",
+--   lazy = true,
+--   ft = "markdown",
+--   dependencies = { "nvim-lua/plenary.nvim" },
+--   opts = {
+--     workspaces = {
+--       {
+--         name = "Paulaner",
+--         path = "~/Dev/Git/obsidian/Paulaner",
+--       },
+--       { -- NOTE: Use obsidian bullet highlighting also for all MD files not only Obsidian workspaces
+--         name = "no-vault",
+--         path = function()
+--           -- alternatively use the CWD:
+--           -- return assert(vim.fn.getcwd())
+--           return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+--         end,
+--         overrides = {
+--           notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of nil
+--           new_notes_location = "current_dir",
+--           templates = {
+--             subdir = vim.NILa,
+--           },
+--           disable_frontmatter = true,
+--         },
+--       },
+--     },
+--   },
+-- }
