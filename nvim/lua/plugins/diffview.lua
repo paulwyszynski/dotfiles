a1a244e4eba984527ccf28f4290a2ed002ae26b8
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-07-25 14:56:59 +0200

    Tested diffview

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,15 @@
+if true then
+  return {}
+end
+-- return {
+--   "sindrets/diffview.nvim",
+--   opts = {
+--     view = {
+--       merge_tool = {
+--         layout = "diff4_mixed",
+--         disable_diagnostic = true,
+--         winbar_info = true,
+--       },
+--     },
+--   },
+-- }
