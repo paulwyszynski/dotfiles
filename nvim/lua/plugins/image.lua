53727d9ba14c72d087b6e15b537d92805e000019
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-08-04 19:29:05 +0200

    Add image support to nvim

---
+++ nvim/lua/plugins/image.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1001, -- this plugin needs to run before anything else
+    opts = {
+      rocks = { "magick" },
+    },
+  },
+  {
+    "3rd/image.nvim",
+    opts = {
+      integrations = {
+        markdown = {
+          clear_in_insert_mode = true,
+          only_render_image_at_cursor = true,
+        },
+      },
+      html = {
+        enabled = true,
+      },
+      css = {
+        enabled = true,
+      },
+      max_width_window_percentage = 50,
+    },
+  },
+}
