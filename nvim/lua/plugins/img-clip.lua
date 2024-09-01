13ff0630105b0b593f14c41bfa2fce0c5a3e316d
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-09-01 12:30:07 +0200

    Disable img-clip

--- nvim/lua/plugins/img-clip.lua
+++ nvim/lua/plugins/img-clip.lua
@@ -1,21 +1,24 @@
-return {
-  "HakonHarnes/img-clip.nvim",
-  event = "VeryLazy",
-  opts = {
-    default = {
-      extension = "avif",
-      process_cmd = "magick - -quality 75 -",
-      dir_path = function()
-        return vim.fn.expand("%:t:r") .. "_img"
-      end,
-      prompt_for_file_name = true,
-      drag_and_drop = {
-        enable = true,
-        insert_mode = true,
-      },
-    },
-  },
-  keys = {
-    { "<leader>v", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-  },
-}
+if true then
+  return {}
+end
+-- return {
+--   "HakonHarnes/img-clip.nvim",
+--   event = "VeryLazy",
+--   opts = {
+--     default = {
+--       extension = "avif",
+--       process_cmd = "magick - -quality 75 -",
+--       dir_path = function()
+--         return vim.fn.expand("%:t:r") .. "_img"
+--       end,
+--       prompt_for_file_name = true,
+--       drag_and_drop = {
+--         enable = true,
+--         insert_mode = true,
+--       },
+--     },
+--   },
+--   keys = {
+--     { "<leader>v", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+--   },
+-- }
