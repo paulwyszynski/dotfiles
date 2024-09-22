20f9fe36df02aa8aa67358c50f8ec747bb5bbab6
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-09-22 22:49:15 +0200

    Nvim Config: Add new key bindings for searching my obsidian vault

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -14,8 +14,22 @@ keymap.set("n", "<C-S-Up>", "<cmd>resize +2<cr>", { desc = "Increase window heig
 keymap.set("n", "<C-S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 keymap.set("n", "<C-S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 keymap.set("n", "<C-S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+keymap.set(
+  "n",
+  "<leader>svt",
+  "<cmd>lua require('telescope.builtin').live_grep({ hidden = false, search_dirs = { '~/Dev/Git/obsidian/Paulaner/'} })<cr>",
+  { desc = "Search Vault for Text" }
+)
+keymap.set(
+  "n",
+  "<leader>svf",
+  "<cmd>lua require('telescope.builtin').find_files({ hidden = false, search_dirs = { '~/Dev/Git/obsidian/Paulaner/' } })<cr>",
+  { desc = "Search Vault for Files" }
+)
+
+-- INFO: Not used anymore, since caps lock is remapped to escape
 
 -- Better escape
-keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
+-- keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
 -- Leads to slow scrolling down inside lazygit
 -- keymap.set("t", "jk", "<C-\\><C-n>", { noremap = true, silent = true })
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,15 +1,9 @@
--- New version of which-key has issues with this config. New defaults using instead.
-if true then
-  return {}
-end
--- return {
---   "folke/which-key.nvim",
---   opts = {
---     defaults = {
---       { ",", desc = "Repeat f, t, F or T N times backwards" },
---       { ";", desc = "Repeat f, t, F or T N times" },
---       { "<C-d>", desc = "Unindent in insert mode" },
---       { "<C-t>", desc = "Indent in insert mode" },
---     },
---   },
--- }
+return {
+  "folke/which-key.nvim",
+  opts = function()
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>sv", desc = "+vault" },
+    })
+  end,
+}
