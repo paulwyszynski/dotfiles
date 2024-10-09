9b7ccc5221e291cff7a88464d6cb33c1247eb8cb
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-10-09 13:49:23 +0200

    Lazy: Add obsidian.nvim

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,34 +1,78 @@
--- Got obsolete since the new markdown.nvim plugin
-if true then
-  return {}
-end
--- return {
---   "epwalsh/obsidian.nvim",
---   lazy = true,
---   ft = "markdown",
---   dependencies = { "nvim-lua/plenary.nvim" },
---   opts = {
---     workspaces = {
---       {
---         name = "Paulaner",
---         path = "~/Dev/Git/obsidian/Paulaner",
---       },
---       { -- NOTE: Use obsidian bullet highlighting also for all MD files not only Obsidian workspaces
---         name = "no-vault",
---         path = function()
---           -- alternatively use the CWD:
---           -- return assert(vim.fn.getcwd())
---           return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
---         end,
---         overrides = {
---           notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of nil
---           new_notes_location = "current_dir",
---           templates = {
---             subdir = vim.NILa,
---           },
---           disable_frontmatter = true,
---         },
---       },
---     },
---   },
--- }
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    workspaces = {
+      {
+        name = "Paulaner",
+        path = "~/Dev/Git/obsidian/Paulaner",
+      },
+    },
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    completion = {
+      -- Set to false to disable completion.
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+    -- Optional, for templates (see below).
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
+    },
+    disable_frontmatter = false,
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+        suffix = tostring(os.time()) .. "-" .. suffix
+      end
+      return suffix
+    end,
+    -- Optional, alternatively you can customize the frontmatter data.
+    ---@return table
+    note_frontmatter_func = function(note)
+      -- Add the title of the note as an alias.
+      if note.title then
+        note:add_alias(note.title)
+      end
+
+      local out = {
+        id = note.id,
+        aliases = {},
+        tags = note.tags,
+        author = "Paul Wyszynski",
+        date = os.date("%Y-%m-%d-%a"),
+      }
+
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+
+      return out
+    end,
+    ui = {
+      enable = false, --NOTE: I'm using markdown.nvim for rendering
+    },
+  },
+}
