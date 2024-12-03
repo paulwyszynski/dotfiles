039686911797d1df33cd04bd6942345bc709251c
Paul Wyszynski <paul.wyszynski@jamitlabs.com>
2024-12-03 22:19:20 +0100

    Nvim: Disable kitty-scrollback

--- nvim/lua/plugins/kitty-scrollback.lua
+++ nvim/lua/plugins/kitty-scrollback.lua
@@ -1,10 +1,14 @@
-return {
-  "mikesmithgh/kitty-scrollback.nvim",
-  enabled = true,
-  lazy = true,
-  cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
-  event = { "User KittyScrollbackLaunch" },
-  config = function()
-    require("kitty-scrollback").setup()
-  end,
-}
+-- INFO: Replaced with tmux
+if true then
+  return {}
+end
+-- return {
+--   "mikesmithgh/kitty-scrollback.nvim",
+--   enabled = true,
+--   lazy = true,
+--   cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+--   event = { "User KittyScrollbackLaunch" },
+--   config = function()
+--     require("kitty-scrollback").setup()
+--   end,
+-- }
