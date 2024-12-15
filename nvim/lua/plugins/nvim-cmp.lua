96136a77860e443aee7473be04f3bb6d70b98d97
Paul Wyszynski <paul.wyszynski@gmail.com>
2024-12-15 17:06:07 +0100

    Nvim: Change nvim-cmp sort order

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,16 +1,14 @@
+-- TODO: Replace this with blink-cmp after obsidian.nvim support for blink-cmp is added
 return {
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local cmp = require("cmp")
       opts.sources = cmp.config.sources({
-        { name = "luasnip" }, -- For luasnip users.
         { name = "nvim_lsp" },
+        { name = "luasnip" }, -- For luasnip users.
         { name = "path" }, -- hrsh7th/cmp-path
         { name = "copilot" },
-        -- { name = "ultisnips" }, -- For ultisnips users.
-        -- { name = "vsnip" }, -- For vsnip users.
-        -- { name = "snippy" }, -- For snippy users.
       }, {
         { name = "buffer" }, -- hrsh7th/cmp-buffer
       })
