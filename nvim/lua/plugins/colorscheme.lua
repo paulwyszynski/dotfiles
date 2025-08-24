e10138be6dfb5d251842bc3b54e1aaacb9b1fdae
Paul Wyszynski <paul.wyszynski@gmail.com>
2025-08-24 11:25:19 +0200

    Nvim: Fix Lazyvim bufferline error

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,5 +6,12 @@ return {
     styles = {
       keywords = { "italic" },
     },
+    -- TODO: Remove this setup fucntion when Lazyvim is updated: https://github.com/LazyVim/LazyVim/issues/6355
+    setup = function()
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+    end,
   },
 }
