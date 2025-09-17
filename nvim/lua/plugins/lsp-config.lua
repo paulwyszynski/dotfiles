dd0d8c6dddb73388e868d3c8cdf7ad04969caafd
Paul Wyszynski <paul.wyszynski@gmail.com>
2025-09-17 21:53:50 +0200

    Lazy: LazyVim 15 release!

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,7 +1,7 @@
 return {
   -- NOTE: MASON INSTALLER: LSPS, FORMATTERS, LINTERS AND DEBUGGERS
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "bash-language-server",
