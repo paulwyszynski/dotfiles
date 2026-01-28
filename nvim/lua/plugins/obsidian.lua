706a864bd1d53130c373eb1a7caed5be3eaa028e
Paul Wyszynski <paul.wyszynski@gmail.com>
2026-01-28 23:44:39 +0100

    Obsidian.nvim: Fix deprecated api

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -26,7 +26,33 @@ return {
       date_format = "%Y-%m-%d-%a",
       time_format = "%H:%M",
     },
-    disable_frontmatter = false,
+    frontmatter = {
+      enabled = true,
+      func = function(note)
+        -- Add the title of the note as an alias.
+        if note.title then
+          note:add_alias(note.title)
+        end
+
+        local out = {
+          id = note.id,
+          aliases = note.aliases,
+          tags = note.tags,
+          author = "Paul Wyszynski",
+          date = os.date("%Y-%m-%d-%a"),
+        }
+
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+
+        return out
+      end,
+    },
     -- Optional, customize how note IDs are generated given an optional title.
     ---@param title string|?
     ---@return string
@@ -49,30 +75,6 @@ return {
     end,
     -- Optional, alternatively you can customize the frontmatter data.
     ---@return table
-    note_frontmatter_func = function(note)
-      -- Add the title of the note as an alias.
-      if note.title then
-        note:add_alias(note.title)
-      end
-
-      local out = {
-        id = note.id,
-        aliases = note.aliases,
-        tags = note.tags,
-        author = "Paul Wyszynski",
-        date = os.date("%Y-%m-%d-%a"),
-      }
-
-      -- `note.metadata` contains any manually added fields in the frontmatter.
-      -- So here we just make sure those fields are kept in the frontmatter.
-      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-        for k, v in pairs(note.metadata) do
-          out[k] = v
-        end
-      end
-
-      return out
-    end,
     -- picker = {
     --   name = "fzf-lua",
     -- },
