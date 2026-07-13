47e19961a263888423fa6a4833a8b8fe9b53b341
Paul Wyszynski <paul.wyszynski@gmail.com>
2026-07-13 23:20:11 +0200

    Obsidian: Remove nvim_cmp and blink (deprecated)

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -13,10 +13,6 @@ return {
     },
     -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
     completion = {
-      -- Set to false to disable completion.
-      nvim_cmp = false,
-      -- Enables completion using blink.cmp
-      blink = true,
       -- Trigger completion at 2 chars.
       min_chars = 2,
     },
